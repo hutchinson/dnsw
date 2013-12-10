da55bec907841b73a6287a72262769ee7a10b094
Dan Hutchinson <d.hutchinson4@googlemail.com>
2013-12-10 22:11:38 +0000

    First look C++11 threads… These will be used to launch resolver tasks on different threads, we then pick the first one that wins.

--- src/resolver/include/resolver/coopresolver.h
+++ src/resolver/include/resolver/coopresolver.h
@@ -2,6 +2,40 @@
 #define COOPRESOVLER_H
 
 #include "resolver/resolver.h"
+#include <condition_variable>
+#include <mutex>
+
+namespace
+{
+  class semaphore
+  {
+    public:
+      semaphore(int count = 0) : 
+        count(count) { }
+
+      void notify()
+      {
+        std::unique_lock<std::mutex> lck(mtx);
+        ++count;
+        cv.notify_one();
+      }
+
+      void wait()
+      {
+        std::unique_lock<std::mutex> lck(mtx);
+
+        while(count == 0){
+            cv.wait(lck);
+        }
+        count--;
+      }
+
+    private:
+      std::mutex mtx;
+      std::condition_variable cv;
+      int count;
+  };
+}
 
 namespace dnsw
 {
@@ -12,6 +46,9 @@ namespace dnsw
       ~coopresolver();
       const std::string resolve(const std::string &name,
                                 dnsw::rr::TYPE record_type);
+
+    private:
+      semaphore m_all_threads_done;
   };
 }
 
--- src/resolver/src/coopresolver.cpp
+++ src/resolver/src/coopresolver.cpp
@@ -1,8 +1,24 @@
 #include "resolver/coopresolver.h"
 
 #include <iostream>
+#include <thread>
+#include <utility>
+#include <chrono>
+#include <functional>
+#include <vector>
+
+namespace
+{
+  void test_function(int id, semaphore & sem)
+  {
+    std::cout << "test function " << id << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    sem.notify();
+  }
+}
 
 dnsw::coopresolver::coopresolver()
+  : m_all_threads_done(-3)
 {
   std::cout << "coopresolver!" << std::endl;
 }
@@ -15,5 +31,20 @@ dnsw::coopresolver::~coopresolver()
 const std::string dnsw::coopresolver::resolve(const std::string &name,
                                          dnsw::rr::TYPE record_type)
 {
+  std::thread t1 (test_function, 1, std::ref(m_all_threads_done));
+  std::thread t2 (test_function, 2, std::ref(m_all_threads_done));
+  std::thread t3 (test_function, 3, std::ref(m_all_threads_done));
+
+  // Wait for all threads...
+  std::cout << "Waiting for threads" << std::endl;
+
+  m_all_threads_done.wait();
+
+  t1.join();
+  t2.join();
+  t3.join();
+
+  std::cout << "All Threads done..." << std::endl;
+
   return "";
 }
